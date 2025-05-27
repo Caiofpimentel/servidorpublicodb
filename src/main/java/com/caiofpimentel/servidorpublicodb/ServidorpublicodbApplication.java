c55e741a3b63ea3ca5e51fe567a5cd5dd082b594
Caio <caio-pimentel@hotmail.com>
2025-05-27 11:03:26 -0300

    Initial commit

---
+++ src/main/java/com/caiofpimentel/servidorpublicodb/ServidorpublicodbApplication.java
@@ -0,0 +1,13 @@
+package com.caiofpimentel.servidorpublicodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorpublicodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorpublicodbApplication.class, args);
+	}
+
+}
